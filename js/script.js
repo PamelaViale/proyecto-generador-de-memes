e80b1206782fad63413266522ed8ffff1453788a
pamelaviale <pamelaviale87@gmail.com>
2023-03-11 21:09:03 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// declaro variables // declaro funciones del proyecto // eventos
+const $ =(selector) => document.querySelector("selector")
+//reveer clase 27 feb dom
+const bodybox = document.querySelector("#bodybox")
+bodybox.style.backgroundImage =  "url('https://img.freepik.com/fotos-premium/bonitos-lindos-gatitos-ilustracion-realista-digital-gato-bebe-cara-frontal-buena-iluminacion_510654-340.jpg')"
+
+// https://cdns3.eltiempo.es/eltiempo/blog/noticias/2021/04/23101047/arboles-1024x682.jpg
+
+
+
+
+
+
+
